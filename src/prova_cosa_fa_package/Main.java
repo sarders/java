00e4fe3cf44fdb00bd39c4e836b5e63524c156cf
protick92 <protick92@gmail.com>
2022-05-19 00:23:43 +0200

    Initial commit

---
+++ src/prova_cosa_fa_package/Main.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    /*
+    *   public: is an access modifier.
+    *   static: we have to jave static for java to find our mathod.
+    *   void: the method dident return any information.
+    *
+    **/
+    public static void main(String[] args) {
+        System.out.println("Hello\nworld!");
+
+        int myFirstInt = 1 + 10 * 5;    // fa prima la moltipicazione
+        System.out.println(myFirstInt);
+        int meno5 = myFirstInt - 5;
+        System.out.println(meno5);
+
+    }
+}
